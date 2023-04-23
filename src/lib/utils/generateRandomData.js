9eb1bb6475d33f2fab9a90435e87199da1988c93
Joshua Singh <51122955+B00MParry@users.noreply.github.com>
2023-04-23 17:44:51 +0200

    fix: reverts generateData function to a .js file to avoid compile errors

---
+++ src/lib/utils/generateRandomData.js
@@ -0,0 +1,52 @@
+function generateData(n) {
+	const names = [
+		'Alice',
+		'Bob',
+		'Charlie',
+		'David',
+		'Eve',
+		'Frank',
+		'Grace',
+		'Heidi',
+		'Ivan',
+		'Julia'
+	]
+	const data = []
+	for (let i = 0; i < n; i++) {
+		let startDate
+		let endDate
+		do {
+			startDate = new Date(
+				Math.floor(Math.random() * 7) + 2020,
+				Math.floor(Math.random() * 12),
+				Math.floor(Math.random() * 28) + 1
+			)
+			endDate = new Date(
+				Math.floor(Math.random() * 7) + 2020,
+				Math.floor(Math.random() * 12),
+				Math.floor(Math.random() * 28) + 1
+			)
+		} while (endDate <= startDate)
+		const Budget = Math.floor(Math.random() * 100000)
+		const id = i + 1
+		const name = names[Math.floor(Math.random() * names.length)]
+		data.push({
+			id,
+			name,
+			startDate: `${startDate
+				.getDate()
+				.toString()
+				.padStart(2, '0')}/${(startDate.getMonth() + 1)
+				.toString()
+				.padStart(2, '0')}/${startDate.getFullYear()}`,
+			endDate: `${endDate
+				.getDate()
+				.toString()
+				.padStart(2, '0')}/${(endDate.getMonth() + 1)
+				.toString()
+				.padStart(2, '0')}/${endDate.getFullYear()}`,
+			Budget
+		})
+	}
+	return data
+}
